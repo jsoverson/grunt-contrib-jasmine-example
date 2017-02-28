43e2f43381b22dd6048d4a9807694a9500e3045e
Roopesh Reddy <cr.reddy@hotmail.com>
2017-02-28 12:35:02 -0700

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,29 +1,31 @@
-{
-  "name": "grunt-contrib-jasmine-example",
-  "description": "Example application using grunt-contrib-jasmine",
-  "version": "0.1.0",
-  "license": "MIT",
-  "homepage": "https://github.com/jsoverson/grunt-contrib-jasmine-example",
-  "author": {
-    "name": "Jarrod Overson",
-    "email": "jsoverson@gmail.com",
-    "url": "http://jarrodoverson.com/"
-  },
-  "repository": {
-    "type": "git",
-    "url": "git://github.com/jsoverson/grunt-contrib-jasmine-example.git"
-  },
-  "scripts": {
-    "test": "grunt test"
-  },
-  "bugs": {
-    "url": "https://github.com/jsoverson/grunt-contrib-jasmine-example/issues"
-  },
-  "devDependencies": {
-    "grunt": "~0.4.0",
-    "grunt-contrib-jasmine": "^1.0.3",
-    "grunt-contrib-jshint": "~0.2.0",
-    "jasmine": "^2.5.3"
-  },
-  "dependencies": {}
-}
+module.exports = function(grunt) {
+  'use strict';
+
+  // Project configuration.
+  grunt.initConfig({
+    jasmine : {
+      src : 'src/**/*.js',
+      options : {
+        specs : 'spec/**/*.js'
+      }
+    },
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'src/**/*.js',
+        'spec/**/*.js'
+      ],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.registerTask('test', ['jshint', 'jasmine']);
+
+  grunt.registerTask('default', ['test']);
+
+};
